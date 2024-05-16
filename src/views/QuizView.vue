b4ce6c8cc23f785b9ed8e1d36bb540a3d3d1cca5
Mayankgera11 <mayank.gera@comprotechnologies.com>
2024-05-16 10:29:34 +0530

    firstcommit

---
+++ src/views/QuizView.vue
@@ -0,0 +1,28 @@
+<script setup>
+import Question from "../components/Question.vue";
+import QuizHeader from "../components/QuizHeader.vue";
+import { useRoute } from "vue-router";
+
+import quizes from "../data/quizes.json";
+
+import { ref } from "vue";
+
+const route = useRoute();
+
+const quizId = parseInt(route.params.id);
+
+const quiz = quizes.find((q) => q.id === quizId);
+
+const currentQuestionIndex = ref(0);
+</script>
+
+<template>
+  <div>
+    <QuizHeader />
+    <div>
+      <Question :question="quiz.questions[currentQuestionIndex]" />
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
